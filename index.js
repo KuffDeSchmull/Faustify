e1d8b31847603ccd9c971221e6b16f0f0abd4046
kuffer <sven.kuffer@rwth-aachen.de>
2021-09-22 15:38:09 +0200

    v14.0.1

--- index.js
+++ index.js
@@ -1,54 +1,59 @@
-// Require the necessary discord.js classes
-const { Client, Intents, VoiceChannel } = require('discord.js');
-const { prefix, token } = require('./config.json');
-const {
-	joinVoiceChannel,
-	createAudioPlayer,
-	createAudioResource,
-	entersState,
-	StreamType,
-	AudioPlayerStatus,
-	VoiceConnectionStatus,
-} = require ('@discordjs/voice');
-
-
-//const { createDiscordJSAdapter } = require ('./adapter');
-//yt playback and search
-const ytdl = require('ytdl-core');
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+const ytdl = require("ytdl-core");
 const yts = require( 'yt-search' )
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
-//const connection;
-//map song queue and guild
+const client = new Discord.Client();
+
 const queue = new Map();
-//check if user is in voice chat and if bot has permissions
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.once("reconnecting", () => {
+  console.log("Reconnecting!");
+});
+
+client.once("disconnect", () => {
+  console.log("Disconnect!");
+});
+
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)){return;};
+
+  const serverQueue = queue.get(message.guild.id);
+
+  if (message.content.startsWith(`${prefix}play`)) {
+    execute(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}skip`)) {
+    skip(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}stop`)) {
+    stop(message, serverQueue);
+    return;
+  } else {
+    message.channel.send("Vache Glift?");
+  }
+});
+
 async function execute(message, serverQueue) {
-    const args = message.content.split(" ");
+  const args = message.content.split(" ");
 
-    //console.log(args)
-    var voiceChannel = message.member.voice.channel;
-    //console.log(voiceChannel.voice)
-    //console.log(voiceChannel.isVoice())
-    var connection;
-    if (!voiceChannel){
-        if( typeof connection !== 'undefined'){
-            connection.destroy();
-        }
-        
-        return message.channel.send(
-            "Du hues keng Moss am Bic!"//you are not in voice chat
-          );
-    }
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel)
+    return message.channel.send(
+      "Du hues keng Moss am Bic"
+    );
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    return message.channel.send(
+      "Ech brauch 2 Bullen Mokka"
+    );
+  }
 
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-      return message.channel.send(
-        "Zwou Bullen Mokka"//bot has not enough permissions
-      );
-      
-    }
-//search for song
-const search = args.shift();
+  const search = args.shift();
 let searchString;
 for (let i = 0; i < args.length; i++){
     searchString = searchString + " " + args[i];
@@ -63,172 +68,81 @@ videos.forEach( function ( v ) {
 } );
 //only link
 
-var songInfo = await ytdl.getInfo(url);
-console.log(songInfo);
-const song = {
- title: songInfo.videoDetails.title,
- url: url,
-};
-//add to queue
-if (!serverQueue) { 
-// Creating the contract for our queue
-const queueContruct = {
-    textChannel: message.channel,
-    voiceChannel: voiceChannel,
-    connection: null,
-    songs: [],
-    volume: 5,
-    playing: true,
+const songInfo = await ytdl.getInfo(url);
+  const song = {
+        title: songInfo.videoDetails.title,
+        url: songInfo.videoDetails.video_url,
    };
-   // Setting the queue using our contract
-   queue.set(message.guild.id, queueContruct);
-   // Pushing the song to our songs array
-   queueContruct.songs.push(song);
-   console.log(song)//debug undefined
-   try {
-    // Here we try to join the voicechat and save our connection into our object.
-    connection = await joinVoiceChannel({
-        channelId: voiceChannel.id,
-        guildId: voiceChannel.guild.id,
-        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-    });
-    queueContruct.connection = connection;
-    // Calling the play function to start a song
-    play(message.guild, queueContruct.songs[0]);//error serverqueue not init
-   } catch (err) {
-    // Printing the error message if the bot fails to join the voicechat
-    console.log(err);
-    console.log("connection failed");
-    queue.delete(message.guild.id);
-    return message.channel.send(err);
-   }
-}else {
- serverQueue.songs.push(song);
- console.log(serverQueue.songs);
- return message.channel.send(`${song.title} leeft dem Ketti mat der Wichsbiischt no`);
-}
-//connect to voice chat of meesenger
-  /*  connection = joinVoiceChannel({
-        channelId: voiceChannel.id,
-        guildId: voiceChannel.guild.id,
-        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-    });
-*/
 
+  if (!serverQueue) {
+    const queueContruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true
+    };
 
-    return;
-}
+    queue.set(message.guild.id, queueContruct);
 
-//play the current cong
-function play(guild, song) {
-console.log("here we are")
-    //serverQueue.textChannel.send(`We are here`);
-    const serverQueue = queue.get(guild.id);
-    //check if queue is empty
-    if (!song) {
-      serverQueue.connection.destroy();
-      queue.delete(guild.id);
-      return;
+    queueContruct.songs.push(song);
+
+    try {
+      var connection = await voiceChannel.join();
+      queueContruct.connection = connection;
+      play(message.guild, queueContruct.songs[0]);
+    } catch (err) {
+      console.log(err);
+      queue.delete(message.guild.id);
+      return message.channel.send(err);
     }
-    //recursive queue playback
-    const dispatcher = serverQueue.connection
-    const player = createAudioPlayer();
-    var stream = ytdl(song.url, { filter: 'audioonly' });
-    var resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
+  } else {
+    serverQueue.songs.push(song);
+    return message.channel.send(`${song.title} leeft dem Ketti mat der Wichsbiischt no`);
+  }
+}
+
+function skip(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "Du hues keng Moss am Bic"
+    );
+  if (!serverQueue)
+    return message.channel.send("Keen Chachacha am Pijama");
+  serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "Du hues keng Moss am Bic"
+    );
     
-    dispatcher.subscribe(player);
-    player.play(resource)//.play(ytdl(song.url))//not a function
-    /*.on("finish", () => {
-        serverQueue.songs.shift();
-        play(guild, serverQueue.songs[0]);
+  if (!serverQueue)
+    return message.channel.send("Keen Chachacha am Pijama");
+    
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.end();
+}
+
+function play(guild, song) {
+  const serverQueue = queue.get(guild.id);
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+
+  const dispatcher = serverQueue.connection
+    .play(ytdl(song.url))
+    .on("finish", () => {
+      serverQueue.songs.shift();
+      play(guild, serverQueue.songs[0]);
     })
     .on("error", error => console.error(error));
-    //dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-    serverQueue.textChannel.send(`Start playing: **${song.title}**`);*/
-    
-    player.on(AudioPlayerStatus.Idle, () => {
-        serverQueue.songs.shift();
-        if(!serverQueue.songs[0]){
-            connection.destroy();
-            return;
-        }
-        stream = ytdl(song.url, { filter: 'audioonly' });
-        resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
-        player.play(resource);
-        serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-    });
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  serverQueue.textChannel.send(`'t As Kiirmes Am Duerf mat: **${song.title}**`);
 }
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready now!');
-    client.user.setPresence({ activities: [{ name: 'the funkiest beats in town' }], status: 'online' })
-});
-
-//functions
-/*
-client.on('interactionCreate', async interaction => {
-
-    console.log("execution triggered");
-    //if message is from bot, the ignore
-
-    if (interaction.author.bot) return;
-    //if message does not begin with command prefix, then ignore
-    if (!interaction.content.startsWith(prefix)) return;
-
-    //commands
-
-    await interaction.reply('Test command')
-    //interaction.send("hola")
-});
-*/
-client.on('message', (message) => {
-	console.log(message.content);
-
-    //if message is from bot, the ignore
-
-    if (message.author.bot) return;
-    //if message does not begin with command prefix, then ignore
-    if (!message.content.startsWith(prefix)) return;
-
-    //commands
-    const serverQueue = queue.get(message.guild.id);
-
-    if (message.content.startsWith(`${prefix}play`)) {
-        execute(message, serverQueue);
-        console.log("finished execution")
-        return;
-    } else if (message.content.startsWith(`${prefix}skip`)) {
-        //skip(message, serverQueue);
-    return;
-    } else if (message.content.startsWith(`${prefix}stop`)) {
-        //stop(message, serverQueue);
-        return;
-    } else {
-        message.channel.send('Vache Glift? Type !help for command list')
-    }
-
-
-
-
-});
-
-client.on('interactionCreate', async interaction => {
-	//if (!interaction.isCommand()) return;
-
-	//const { commandName } = interaction;
-
-	/*if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
-	}*/
-    await interaction.reply('2 Bulle Mokka');
-});
-
-//
-// Login to Discord with your client's token
-client.login(token)
-
+client.login(token);
